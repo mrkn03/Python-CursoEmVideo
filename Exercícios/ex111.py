eb5512b574c0e9f1fb38e416dfa2b53251690f8a
Marcos Vinicios Fernandes <marcos.fernandes0324@gmail.com>
2024-07-15 23:10:12 -0300

    Update

--- Exercícios/ex111.py
+++
@@ -1,2 +0,0 @@
-"""Crie um pacote chamado utilidadesCeV que tenha dois modulos internos chamado moeda e dado. Transfira todas as funçoes
-utilizadas nos desafios 107, 108 e 109 para o primeiro pacote e mantenha tudo funcionando"""
